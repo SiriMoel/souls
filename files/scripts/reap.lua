376a759d4e38e69b2cb4a2f08bfb85988f32072d
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2023-06-22 11:43:03 +1000

    i think i fixed souls

--- files/scripts/reap.lua
+++ files/scripts/reap.lua
@@ -3,7 +3,7 @@ dofile_once("mods/moles_things/files/scripts/souls.lua")
 
 function death(damage_type_bit_field, damage_message, entity_thats_responsible, drop_items)
     local entity = GetUpdatedEntityID()
-    local herd_id_number = ComponentGetValue2( EntityGetFirstComponent( entity_id, "GenomeDataComponent" ) or 0, "herd_id")
+    local herd_id_number = ComponentGetValue2( EntityGetFirstComponent( entity, "GenomeDataComponent" ) or 0, "herd_id")
     local herd_id = HerdIdToString(herd_id_number)
     
     if not table.contains(soul_types, herd_id) then return end

879a1edec6c19e648415ece5b75d9fea553f6b25
Mir Sole <zburn101@gmail.com>
2024-01-02 19:12:34 +1100

    soul door (it is so strange)

--- files/biome/souldoor/souldoor.lua
+++ files/biome/souldoor/souldoor.lua
@@ -13,6 +13,9 @@ local targets = EntityGetInRadiusWithTag(x, y, radius, "card_action")
 
 --local keys = EntityGetInRadiusWithTag(x, y, radius, "kupoli_door_key")
 
+-- the separate pools dont work in a way i dont quite understand
+-- also i keep getting the same order of spells im so confused
+
 local spell_projectiles = {}
 local pool_projectiles = {
     "KUPOLI_SOUL_BLAST",
@@ -20,10 +23,6 @@ local pool_projectiles = {
     "KUPOLI_REAPER_BLADE",
     "KUPOLI_SOUL_EXPLOSION",
     "KUPOLI_SOUL_GUARD",
-}
-
-local spell_modifiers = {}
-local pool_modifiers = {
     "KUPOLI_SOUL_SPEED",
     "KUPOLI_REAPING_SHOT",
     "KUPOLI_SOULS_TO_POWER",
@@ -43,18 +42,14 @@ end]]--
 
 if #targets > 0 and targets ~= nil then
     for i,v in ipairs(targets) do
-        if table.contains(spell_projectiles, v) or table.contains(spell_modifiers, v) then return end
+        if table.contains(spell_projectiles, v) --[[or table.contains(spell_modifiers, v)]] then return end
         if not EntityHasTag(EntityGetParent(v), "wand") and EntityGetComponent(EntityGetParent(v), "AbilityComponent") == nil and EntityGetName(EntityGetParent(v)) ~= "inventory_full" then
             if EntityGetFirstComponentIncludingDisabled(v, "ItemActionComponent") ~= nil then
                 --string = string .. "," .. ComponentGetValue2(EntityGetFirstComponentIncludingDisabled(v, "ItemActionComponent") or 0, "action_id")
                 for i,action in ipairs(actions) do
                     if action.id == ComponentGetValue2(EntityGetFirstComponentIncludingDisabled(v, "ItemActionComponent") or 0, "action_id") then
                         --if table.contains(spell_projectiles, v) or table.contains(spell_modifiers, v) then return end
-                        if action.type == ACTION_TYPE_PROJECTILE or action.type == ACTION_TYPE_STATIC_PROJECTILE then
-                            table.insert(spell_projectiles, v)
-                        elseif action.type == ACTION_TYPE_MODIFIER then
-                            table.insert(spell_modifiers, v)
-                        end
+                        table.insert(spell_projectiles, v)
                     end
                 end
             end
@@ -63,7 +58,6 @@ if #targets > 0 and targets ~= nil then
     --GamePrint(string)  
 end
 
--- projectiles
 if #spell_projectiles >= 1 then
     local light_1 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_1")[1]
     EntitySetComponentsWithTagEnabled( light_1, "kupoli_exchanger_light_proj", true )
@@ -113,56 +107,4 @@ if #spell_projectiles >= 5 then
     EntitySetComponentsWithTagEnabled( light_3, "kupoli_exchanger_light_proj", false )
     local light_4 = EntityGetInRadiusWithTag(x, y, 80, "kupoli_exchanger_light_4")[1]
     EntitySetComponentsWithTagEnabled( light_4, "kupoli_exchanger_light_proj", false )
-end
-
--- modifiers
-if #spell_modifiers >= 1 then
-    local light_1 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_1")[1]
-    EntitySetComponentsWithTagEnabled( light_1, "kupoli_exchanger_light_mod", true )
-else
-    local light_1 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_1")[1]
-    EntitySetComponentsWithTagEnabled( light_1, "kupoli_exchanger_light_mod", false )
-end
-
-if #spell_modifiers >= 2 then
-    local light_2 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_2")[1]
-    EntitySetComponentsWithTagEnabled( light_2, "kupoli_exchanger_light_mod", true )
-else
-    local light_2 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_2")[1]
-    EntitySetComponentsWithTagEnabled( light_2, "kupoli_exchanger_light_mod", false )
-end
-
-if #spell_modifiers >= 3 then
-    local light_3 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_3")[1]
-    EntitySetComponentsWithTagEnabled( light_3, "kupoli_exchanger_light_mod", true )
-else
-    local light_3 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_3")[1]
-    EntitySetComponentsWithTagEnabled( light_3, "kupoli_exchanger_light_mod", false )
-end
-
-if #spell_modifiers >= 4 then
-    local light_4 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_4")[1]
-    EntitySetComponentsWithTagEnabled( light_4, "kupoli_exchanger_light_mod", true )
-else
-    local light_4 = EntityGetInRadiusWithTag(x, y, 150, "kupoli_exchanger_light_4")[1]
-    EntitySetComponentsWithTagEnabled( light_4, "kupoli_exchanger_light_mod", false )
-end
-
-if #spell_modifiers >= 5 then
-    for i,v in ipairs(spell_modifiers) do
-        EntityKill(v)
-    end
-
-    CreateItemActionEntity(pool_modifiers[math.random(1,#pool_modifiers)], x, y)
-
-    spell_modifiers = {}
-
-    local light_1 = EntityGetInRadiusWithTag(x, y, 80, "kupoli_exchanger_light_1")[1]
-    EntitySetComponentsWithTagEnabled( light_1, "kupoli_exchanger_light_mod", false )
-    local light_2 = EntityGetInRadiusWithTag(x, y, 80, "kupoli_exchanger_light_2")[1]
-    EntitySetComponentsWithTagEnabled( light_2, "kupoli_exchanger_light_mod", false )
-    local light_3 = EntityGetInRadiusWithTag(x, y, 80, "kupoli_exchanger_light_3")[1]
-    EntitySetComponentsWithTagEnabled( light_3, "kupoli_exchanger_light_mod", false )
-    local light_4 = EntityGetInRadiusWithTag(x, y, 80, "kupoli_exchanger_light_4")[1]
-    EntitySetComponentsWithTagEnabled( light_4, "kupoli_exchanger_light_mod", false )
 end

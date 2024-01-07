f317a6e62e4cceea01cbb4d3f2a0cf6acb526d29
Mir Sole <zburn101@gmail.com>
2024-01-08 00:47:42 +1100

    creature tweaks

--- files/scripts/biome/crypt.lua
+++ files/scripts/biome/crypt.lua
@@ -1,5 +1,5 @@
 table.insert(g_big_enemies, {
-    prob   		= 0.01,
+    prob   		= 0,
     min_count	= 1,
     max_count	= 1,
     entity 	= "data/entities/animals/kupoli_soul_angry.xml"
--- files/scripts/biome/wizardcave.lua
+++ files/scripts/biome/wizardcave.lua
@@ -1,24 +1,24 @@
 table.insert(g_small_enemies, {
-    prob   		= 0.1,
+    prob   		= 0.05,
 	min_count	= 1,
 	max_count	= 1,
 	entity 	= "data/entities/animals/kupoli_puppet_master.xml"
 })
 
 table.insert(g_big_enemies, {
-    prob   		= 0.03,
+    prob   		= 0.01,
     min_count	= 1,
     max_count	= 1,
     entity 	= "data/entities/animals/kupoli_soul_angry.xml",
 })
 table.insert(g_big_enemies, {
-	prob   		= 0.1,
+	prob   		= 0.05,
 	min_count	= 1,
 	max_count	= 1,    
 	entities 	= { "data/entities/animals/kupoli_puppet_master.xml", "data/entities/animals/wizard_tele.xml" },
 })
 table.insert(g_big_enemies, {
-	prob   		= 0.02,
+	prob   		= 0.01,
 	min_count	= 1,
 	max_count	= 1,    
 	entities 	= { "data/entities/animals/kupoli_puppet_master.xml", "data/entities/animals/wizard_tele.xml", "data/entities/animals/wizard_dark.xml", "data/entities/animals/wizard_poly.xml", "data/entities/animals/wizard_swapper.xml", "data/entities/animals/wizard_neutral.xml", "data/entities/animals/wizard_twitchy.xml", "data/entities/animals/wizard_returner.xml", "data/entities/animals/wizard_hearty.xml", "data/entities/animals/wizard_weaken.xml", "data/entities/animals/wizard_homing.xml" },

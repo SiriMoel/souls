cb56a8a9e6c76e085f8f4e03af4640f99f6250a7
Mir Sole <zburn101@gmail.com>
2024-01-30 20:17:20 +1100

    funny things

--- files/scripts/drop_money_append.lua
+++ files/scripts/drop_money_append.lua
@@ -1,9 +1,111 @@
 dofile_once( "data/scripts/game_helpers.lua" )
 dofile_once("mods/tales_of_kupoli/files/scripts/utils.lua")
-dofile_once("mods/tales_of_kupoli/files/scripts/biome_things.lua")
 dofile_once("mods/tales_of_kupoli/files/scripts/souls.lua")
 
 local do_money_drop_old = do_money_drop
+
+local biomethings = {
+    {
+        biome = "$biome_coalmine",
+        soul = "zombie",
+        wand = "",
+    },
+    {
+        biome = "$biome_coalmine_alt",
+        soul = "zombie",
+        wand = "",
+    },
+    {
+        biome = "$biome_excavationsite",
+        soul = "bat",
+        wand = "",
+    },
+    {
+        biome = "$biome_fungicave",
+        soul = "fungus",
+        wand = "",
+    },
+    {
+        biome = "$biome_snowcave",
+        soul = "orcs",
+        wand = "",
+    },
+    {
+        biome = "$biome_snowcastle",
+        soul = "orcs",
+        wand = "",
+    },
+    {
+        biome = "$biome_rainforest",
+        soul = "spider",
+        wand = "",
+    },
+    {
+        biome = "$biome_rainforest_dark",
+        soul = "spider",
+        wand = "",
+    },
+    {
+        biome = "$biome_vault",
+        soul = "",
+        wand = "",
+    },
+    {
+        biome = "$biome_crypt",
+        soul = "mage",
+        wand = "",
+    },
+    {
+        biome = "$biome_gold",
+        soul = "gilded",
+        wand = "",
+    },
+    {
+        biome = "$biome_winter",
+        soul = "orcs",
+        wand = "",
+    },
+    {
+        biome = "$biome_tower",
+        soul = "ghost",
+        wand = "",
+    },
+    {
+        biome = "$biome_secret_lab",
+        soul = "mage",
+        wand = "",
+    },
+    {
+        biome = "$biome_vault_frozen",
+        soul = "",
+        wand = "",
+    },
+    {
+        biome = "$biome_wandcave",
+        soul = "mage",
+        wand = "",
+    },
+    {
+        biome = "$biome_wizardcave",
+        soul = "mage",
+        wand = "",
+    },
+    {
+        biome = "$biome_liquidcave",
+        soul = "mage",
+        wand = "",
+    },
+    {
+        biome = "$biome_winter_caves",
+        soul = "ghost",
+        wand = "",
+    },
+    {
+        biome = "$biome_fun",
+        soul = "fungus",
+        wand = "",
+    },
+}
 
 function do_money_drop( amount_multiplier, trick_kill )
 
@@ -33,7 +135,7 @@ function do_money_drop( amount_multiplier, trick_kill )
                         if biomethings[i].soul ~= "" then
                             whichtype = biomethings[i].soul
                         else
-                            whichtype = "friendly"
+                            whichtype = "hiisi"
                         end
                     end
                 end

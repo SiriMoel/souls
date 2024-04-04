1853a15312f3cf4a189a84e846c44d0ec0ebb486
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2024-04-04 14:17:05 +1100

    changes

---
+++ files/scripts/desert_append.lua
@@ -0,0 +1,26 @@
+dofile_once("mods/tales_of_kupoli/files/scripts/utils.lua")
+dofile_once("data/scripts/director_helpers.lua")
+dofile_once("data/scripts/biome_scripts.lua")
+
+local init_old = init
+
+math.randomseed(x, y + tonumber(StatsGetValue("world_seed")))
+
+function init(x, y, w, h)
+    init_old(x, y, w, h)
+    print("testing - desert init")
+
+    local lootorb = "mods/tales_of_kupoli/files/entities/arch_orb/lootorb.xml"
+
+    if y > 10 and y < 200 then
+
+        local orb_x = x
+        local orb_y = y
+
+        for i=1,6 do
+            EntityLoad(lootorb, orb_x, orb_y)
+            orb_x = math.random(orb_x - 256, orb_x + 256)
+            orb_y = math.random(orb_y - 256, orb_y + 256)
+        end
+    end
+end

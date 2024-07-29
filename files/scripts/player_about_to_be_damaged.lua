264b0bf85e866f292dfb13cb1265a609bc4e28a0
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2024-07-29 12:08:28 +1000

    small fix

--- files/scripts/player_about_to_be_damaged.lua
+++ files/scripts/player_about_to_be_damaged.lua
@@ -32,7 +32,7 @@ function damage_about_to_be_received(damage, x, y, entity_thats_responsible, cri
     if GameHasFlagRun("kupoli_phylactery_done") then
         local phylactery_points = tonumber(GlobalsGetValue("kupoli_phylactery_points", "0"))
 
-        if tonumber(GlobalsGetValue("kupoli_phylactery_points_used")) == 250 then
+        if tonumber(GlobalsGetValue("kupoli_phylactery_points_used", "0")) == 250 then
             EntityAddTag(player, "kupoli_lich")
         end
     

56fcd0ed1e8675826095c6de133403957e26f289
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2024-08-09 12:32:44 +1000

    enemies bug fix

--- files/scripts/biome/crypt.lua
+++ files/scripts/biome/crypt.lua
@@ -2,5 +2,5 @@ table.insert(g_big_enemies, {
     prob   		= 0,
     min_count	= 1,
     max_count	= 1,
-    entity 	= "data/entities/animals/kupoli_soul_angry.xml"
+    entity 	= "data/entities/animals/moldos_soul_angry.xml"
 })
--- files/scripts/biome/wizardcave.lua
+++ files/scripts/biome/wizardcave.lua
@@ -2,30 +2,30 @@ table.insert(g_small_enemies, {
     prob   		= 0.05,
 	min_count	= 1,
 	max_count	= 1,
-	entity 	= "data/entities/animals/kupoli_puppet_master.xml"
+	entity 	= "data/entities/animals/moldos_puppet_master.xml"
 })
-table.insert(g_small_enemies, {
+--[[table.insert(g_small_enemies, {
     prob   		= 0.04,
 	min_count	= 1,
 	max_count	= 1,
 	entity 	= "data/entities/animals/kupoli_tank_mage.xml"
-})
+})]]
 
 table.insert(g_big_enemies, {
     prob   		= 0.01,
     min_count	= 1,
     max_count	= 1,
-    entity 	= "data/entities/animals/kupoli_soul_angry.xml",
+    entity 	= "data/entities/animals/moldos_soul_angry.xml",
 })
 table.insert(g_big_enemies, {
 	prob   		= 0.02,
 	min_count	= 1,
 	max_count	= 1,    
-	entities 	= { "data/entities/animals/kupoli_puppet_master.xml", "data/entities/animals/wizard_tele.xml", "data/entities/animals/kupoli_tank_mage.xml" },
+	entities 	= { "data/entities/animals/moldos_puppet_master.xml", "data/entities/animals/wizard_tele.xml", "data/entities/animals/kupoli_tank_mage.xml" },
 })
-table.insert(g_big_enemies, {
+--[[table.insert(g_big_enemies, {
     prob   		= 0.02,
 	min_count	= 2,
 	max_count	= 3,
 	entity 	= "data/entities/animals/kupoli_tank_mage.xml"
-})
+})]]

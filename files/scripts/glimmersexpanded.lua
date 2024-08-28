eacc573b3b39c8c4f7c8c9ba7134f9b5840ac349
Mir Sole <zburn101@gmail.com>
2024-08-28 18:32:41 +1000

    fixed glimmers expanded compat

--- files/scripts/glimmersexpanded.lua
+++ files/scripts/glimmersexpanded.lua
@@ -1,3 +1,3 @@
 dofile_once("mods/souls/files/scripts/utils.lua")
 
-addGlimmer("Soul Blood", "Gives your projectile a soulful sparkly trail", {"soul_blood_1", "soul_blood_perfect"}, "mods/souls/files/spell_icons/glimmer_soul_blood", 8, "1,2,3,4,5,6", 4.5)
+addGlimmer("Soul Blood", "Gives your projectile a soulful sparkly trail", {"soul_blood_1", "soul_blood_perfect"}, "mods/souls/files/spell_icons/glimmer_soul_blood.png", 8, "1,2,3,4,5,6", 4.5)

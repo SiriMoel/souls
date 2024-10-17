1d7d3113138c6eb053569d770e2d7c6b15cbb832
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2024-10-17 14:38:36 +1100

    removed redundant phylactery things

--- files/phylactery/spawn.lua
+++
@@ -1,9 +0,0 @@
-dofile_once("mods/souls/files/scripts/utils.lua")
-
-local altar = GetUpdatedEntityID()
-local x, y = EntityGetTransform(altar)
-
-LoadPixelScene( "mods/souls/files/phylactery/scene.png", "mods/souls/files/phylactery/visual.png", x-256, y-256, "mods/souls/files/phylactery/background.png", true )
-
-x = x
-y = y - 60

2cdc80e2c033c7a7fdb747f089a7abacdee093f7
Mir Sole <zburn101@gmail.com>
2024-12-19 12:34:59 +1100

    fixed soul crystal not spawning boss issue

--- files/entities/items/_soulcrystals/spawn_boss.lua
+++ files/entities/items/_soulcrystals/spawn_boss.lua
@@ -1,16 +1,16 @@
 dofile_once("mods/souls/files/scripts/utils.lua")
 dofile_once("mods/souls/files/scripts/souls.lua")
 
-local entity = GetUpdatedEntityID()
-local comp_mi = EntityGetFirstComponentIncludingDisabled(entity, "MaterialInventoryComponent") or 0
-local comp_ms = EntityGetFirstComponentIncludingDisabled(entity, "MaterialSuckerComponent") or 0
-local x, y = EntityGetTransform(entity)
-
-function kick( entity_who_kicked )
+function kick(entity_who_kicked)
+    local entity = GetUpdatedEntityID()
+    local root = EntityGetRootEntity(entity)
+    local comp_mi = EntityGetFirstComponentIncludingDisabled(entity, "MaterialInventoryComponent") or 0
+    local x, y = EntityGetTransform(entity)
+    if root ~= entity then return end
     local amount_soulblood = ComponentGetValue2(comp_mi, "count_per_material_type")[CellFactory_GetType("soul_blood_1") + 1] or 0 --tostring(CellFactory_GetType("soul_blood_1"))
     --GamePrint(tostring(amount_soulblood))
     if amount_soulblood >= 300 then
-        GamePrint("Revived a boss!")
+        GamePrintImportant("REVIVAL COMPLETE!", "The Gods watch intently...", "mods/souls/files/souls_decoration.png")
         EntityKill(entity)
         EntityLoad(ComponentGetValue2(EntityGetFirstComponentIncludingDisabled(entity, "VariableStorageComponent", "boss") or 0, "value_string"), x, y)
     end
